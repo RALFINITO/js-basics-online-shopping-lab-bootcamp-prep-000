1a7513aafdd4d381f6fadbb522cc17523f2e2eda
rafaelalfinito@hotmail.com <rafaelalfinito@hotmail.com>
2018-05-04 21:45:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,26 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = parseInt(100*Math.random())
+ var itemObj = {itemName: item, itemPrice: price}
+ cart.push(itemObj)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  var ret
+  if (cart.length === 0){
+    ret = "Your shopping cart is empty."
+  }else{
+    ret = "In your cart, you have "
+    for(i=0; i<cart.length;i++){
+      ret = ret + cart[i] +", "
+    }
+    ret = ret.slice(0, ret.length-2);
+  }
+  console.log(ret)
+  return ret
 }
 
 function total() {
